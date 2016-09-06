4df08a8ed053c1d8f850180a2512a90c641f45cd
etmoore <etmoore@gmail.com>
2016-09-05 22:08:37 -0600

    alert the user to take a break 3 times

--- take-a-break.py
+++ take-a-break.py
@@ -1,8 +1,8 @@
 import time
 import webbrowser
 
-# wait 2 hours
-time.sleep(2);
-
-# when the counter reaches a time divisible by 2 hours, play song
-webbrowser.open("https://www.youtube.com/watch?v=D1sZ_vwqwcE")
+x = 0
+while x < 3:
+    time.sleep(2);
+    webbrowser.open("https://www.youtube.com/watch?v=D1sZ_vwqwcE")
+    x += 1
